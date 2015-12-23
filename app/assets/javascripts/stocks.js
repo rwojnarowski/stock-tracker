14be8194511e7c3ce2c64b8bac8fc848f134b487
rwojnarowski <radziu92@gmail.com>
2015-12-23 21:14:17 +0100

    follow/remove stocks

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,7 +1,7 @@
 var init_stock_lookup;
 
 init_stock_lookup = function() {
-  $('#stock-lookup-form').on('ajax:before', function(event,data,status){
+  $('#stock-lookup-form').on('ajax:before', function(event, data, status){
     show_spinner();
   });
 
@@ -17,7 +17,7 @@ init_stock_lookup = function() {
   $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
     hide_spinner();
     $('#stock-lookup-results').replaceWith(' ');
-    $('#stock-lookup-errors').replaceWith('Stock was not found');
+    $('#stock-lookup-errors').replaceWith('Stock was not found.');
   });
 }
 
